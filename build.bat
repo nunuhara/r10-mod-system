1263e3c3523ea4cefc2714ad6af8c939f764bd62
Nunuhara Cabbage <nunuhara@haniwa.technology>
2021-11-20 16:16:36 -0800

    Implement pact editing support
    Requires alice-tools 0.11.1

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 IF NOT EXIST src.ain (
 	COPY ..\Rance10.ain src.ain
 )
+IF NOT EXIST srcpact.afa (
+	COPY ..\Rance10Pact.afa srcpact.afa
+)
 
 alice-tools\alice.exe project build Rance10.pje
 
--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 if [ ! -f src.ain ]; then
     cp ../Rance10.ain ./src.ain || exit 1
 fi
+if [ ! -f srcpact.afa ]; then
+    cp ../Rance10Pact.afa ./srcpact.afa || exit 1
+fi
 
 alice project build "Rance10.pje"
